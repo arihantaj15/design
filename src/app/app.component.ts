c2fcace4cb285df5b6d9a16fcf71f2d43cb89604
arihantaj15 <jain.ari15@gmail.com>
2021-06-22 01:15:27 +0530

    change2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { ChartType, ChartOptions } from 'chart.js';
-import { SingleDataSet, Label } from 'ng2-charts';
+import { ChartType, ChartOptions, ChartDataSets } from 'chart.js';
+// import { ChartDataSets, ChartOptions } from 'chart.js';
+import { SingleDataSet, Label, Color } from 'ng2-charts';
 
 @Component({
   selector: 'app-root',
@@ -15,8 +16,26 @@ export class AppComponent  {
   public pieChartOptions: ChartOptions = {
     responsive: true,
   };
-  public pieChartLabels: Label[] = [['Download', 'Sales'], ['In', 'Store', 'Sales'], 'Mail Sales'];
-  public pieChartData: SingleDataSet = [300, 500, 100];
+  public lineChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+  ];
+  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions: ChartOptions  = {
+    responsive: true,
+  };
+  public lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,0,0,0.3)',
+    },
+  ];
+  public lineChartLegend = true;
+  public lineChartType: ChartType = 'line';
+  public lineChartPlugins = [];
+  
+
+  public pieChartLabels: Label[] = [['Feedback'], ['Shortlisted'], 'Rejected'];
+  public pieChartData: SingleDataSet = [450, 250, 300];
   public pieChartType: ChartType = 'pie';
   public pieChartLegend = true;
   public pieChartPlugins = [];
